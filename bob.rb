641bf50afa4a66bf826c4a5f5ee19ee1feb3d19a
Kate Walters <kawalters01@gmail.com>
2016-06-20 17:21:16 -0500

    Refactored version

--- bob.rb
+++ bob.rb
@@ -1,21 +1,25 @@
 class Bob
+  QUESTION = "?"
+  EMPTY = ""
 
   def hey(remark)
-    if remark.gsub(/\s+/, "") == ""
+    if remark.gsub(/\s+/, EMPTY) == EMPTY
       'Fine. Be that way!'
     elsif remark.gsub(/(\d|\W^|,|\s)/, '').empty?
       'Whatever.'
-    elsif remark.end_with?("?") && remark == remark.capitalize || remark == remark.downcase# if a string ends in ? logic goes here
+    elsif asking_question?(remark) && remark == remark.capitalize || remark == remark.downcase# if a string ends in ? logic goes here
       'Sure.'
     elsif remark == remark.upcase
       'Whoa, chill out!'
-  #  elsif remark == ""
-  #    'Fine. Be that way!'
-    elsif remark.end_with?("?")
+    elsif asking_question?(remark)
       'Sure.'
     else
       'Whatever.'
     end
+  end
+
+  def asking_question?(remark)
+    remark.end_with?(QUESTION)
   end
 end
 
